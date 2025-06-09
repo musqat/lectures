b014d24cca4ceaafffbd6f2444f71577b6945d86
musqat <hjs90561@naver.com>
2025-06-10 01:22:16 +0900

    build Cards, Loans

---
+++ MicroService/section2/cards/src/main/java/com/muscat/cards/CardsApplication.java
@@ -0,0 +1,40 @@
+package com.muscat.cards;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Cards microservice REST API Documentation",
+        description = "Bank Loans microservice REST API Documentation",
+        version = "v1",
+        contact = @Contact(
+            name = "Muscat Han",
+            email = "muscat@example.com",
+            url = "https://github.com/"
+        ),
+        license = @License(
+            name = "Apache 2.0",
+            url = "https://github.com/"
+        )
+    ),
+    externalDocs = @ExternalDocumentation(
+        description = "Cards microservice REST API Documentation",
+        url = "https://github.com/swagger-ui,html"
+    )
+)
+public class CardsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CardsApplication.class, args);
+  }
+
+}
