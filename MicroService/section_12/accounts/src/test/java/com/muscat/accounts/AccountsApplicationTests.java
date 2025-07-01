95425a9bee1c9d655e97d26732c6db1f4e2532fb
musqat <hjs90561@naver.com>
2025-07-02 05:04:11 +0900

    Microservice Security Using OAuth2

---
+++ MicroService/section_12/accounts/src/test/java/com/muscat/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muscat.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
