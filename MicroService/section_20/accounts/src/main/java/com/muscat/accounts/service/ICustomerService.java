92117dc11f34b21aa38eaa1d2e0552d1c89b359f
musqat <hjs90561@naver.com>
2025-07-12 02:48:08 +0900

    Spring boot BOM & shared Libraries in MicroServices

---
+++ MicroService/section_20/accounts/src/main/java/com/muscat/accounts/service/ICustomerService.java
@@ -0,0 +1,13 @@
+package com.muscat.accounts.service;
+
+
+import com.muscat.accounts.dto.CustomerDetailsDto;
+
+public interface ICustomerService {
+
+  /**
+   * @param mobileNumber - Input Mobile Number
+   * @return Customer Details based on a given mobileNumber
+   */
+  CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId);
+}
---
+++ MicroService/section_20/mus-bom/src/main/java/com/muscat/musbom/MusBomApplication.java
@@ -0,0 +1,13 @@
+package com.muscat.musbom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusBomApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MusBomApplication.class, args);
+  }
+
+}
