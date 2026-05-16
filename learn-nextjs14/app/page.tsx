abb97ada164d675e96fac048f305f0e686f7d0a1
muscat207 <hjs90561@gmail.com>
2026-05-16 16:24:35 +0900

    next.js 2.3

---
+++ learn-nextjs14/app/page.tsx
@@ -0,0 +1,9 @@
+import Navigation from "../components/navigation"
+
+export default function Tomato(){
+    return <div>
+            <Navigation />
+            <h1>Hello </h1>
+            </div>
+}
+
