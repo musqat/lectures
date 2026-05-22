bf5d3f1d575e3a882495fc997b9c86ea3a1a2485
muscat207 <hjs90561@gmail.com>
2026-05-23 00:53:12 +0900

    3 Data Fetching finished

--- learn-nextjs14/app/(movies)/movies/[id]/page.tsx
+++ learn-nextjs14/app/(movies)/movies/[id]/page.tsx
@@ -1,8 +1,20 @@
-export default function MoviesDetail({
+import { Suspense } from "react";
+import MovieVideos from "../../../../components/movie-videos";
+import MovieInfo from "../../../../components/movie-info";
+
+export default async function MoviesDetail({
      params: { id },
     }: {
      params: { id:string };
 
     }) {
-    return <h1>Movie {id}</h1>
+    return <div>
+        <h3>Movie detail page</h3>
+        <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id}/>
+        </Suspense>
+        <Suspense fallback={<h1>Loading movie video</h1>}>
+        <MovieVideos id={id}/>
+        </Suspense>
+    </div>
 }
